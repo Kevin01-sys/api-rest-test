2691b53430027aeddd0f08bcc5b709626720b318
Kevin Chocano <kevinchocano@gmail.com>
2022-03-16 20:46:39 -0300

    se cambio la contrasenia de la base de datos y se añadio un registro a movies

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const mysqlConnection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'password',
+  password: '1234',
   database: 'company',
   multipleStatements: true
 });
